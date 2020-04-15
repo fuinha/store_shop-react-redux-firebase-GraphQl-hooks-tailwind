fc21734216c8e13e82069ccce91757b97fa6c73d
Mohammed-Abdelhady <mohammed_abdelhady97@hotmail.com>
2020-04-15 10:47:12 +0200

    styling menu-item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import './menu-item.styles.scss'
-const MenuItem = ({title,  imageUrl, size}) => {
-    return (
-      <div style={ {
-          backgroundImage: `url(${imageUrl})`
-        }} className={`${size} menu-item`}>
-          <div className='content'>
-                <h2 className='title'>{ title}</h2>
-                <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-    )
-}
+import React from 'react';
+import './menu-item.styles.scss';
+const MenuItem = ({ title, imageUrl, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className='content'>
+        <h2 className='title'>{title}</h2>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
 
-export default MenuItem
+export default MenuItem;
